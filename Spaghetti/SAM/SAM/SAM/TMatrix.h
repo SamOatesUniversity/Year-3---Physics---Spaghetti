bb069ed63716328feaf8ee3a9d9de8b8f906bed0
Sam Oates <sam@samoatesgames.com>
2013-01-12 13:04:25 +0000

    Added Zero() and Identity() methods to the matrix class.

--- Spaghetti/SAM/SAM/SAM/TMatrix.h
+++ Spaghetti/SAM/SAM/SAM/TMatrix.h
@@ -16,17 +16,31 @@ namespace SAM
 																	//! Class constructor
 																	TMatrix()
 																	{
+																		Identity();
+																	}
+
+																	//! Class destructor
+																	~TMatrix();
+
+																	//! Change the matrix to an identity matrix
+		void														Identity()
+																	{
 																		// can only default to identity matrix, if we are a square matrix
 																		if (COLUMNS == ROWS)
 																		{
 																			unsigned int columnIndex = 0;
 																			for (int rowIndex = 0; rowIndex < ROWS; ++rowIndex)
-																				m_element[rowIndex][columnIndex++] = static_cast<Type>(1);
+																				m_element[rowIndex][columnIndex++] = static_cast<TYPE>(1);
 																		}
 																	}
 
-																	//! Class destructor
-																	~TMatrix();
+																	//! Change the matrix to a zero matrix
+		void														Zero()
+																	{
+																		for (int rowIndex = 0; rowIndex < ROWS; ++rowIndex)
+																			for (int columnIndex = 0; columnIndex < COLUMNS; ++columnIndex)
+																				m_element[rowIndex][columnIndex] = static_cast<TYPE>(0);
+																	}
 	};
 
 	//! Matrix 2x2
@@ -41,9 +55,29 @@ namespace SAM
 																	//! Class constructor
 																	TMatrix()
 																	{
+																		Identity();
+																	}
+
+																	//! overload the subscript operator
+		TVector<TYPE, 2>											&operator[] (const unsigned int index)
+																	{
+																		return m_element[index];
+																	}
+
+																	//! Change the matrix to an identity matrix
+		void														Identity()
+																	{																
 																		unsigned int columnIndex = 0;
 																		for (int rowIndex = 0; rowIndex < 2; ++rowIndex)
 																			m_element[rowIndex][columnIndex++] = static_cast<TYPE>(1);
+																	}
+
+																	//! Change the matrix to a zero matrix
+		void														Zero()
+																	{
+																		for (int rowIndex = 0; rowIndex < 2; ++rowIndex)
+																			for (int columnIndex = 0; columnIndex < 2; ++columnIndex)
+																				m_element[rowIndex][columnIndex] = static_cast<TYPE>(0);
 																	}
 	};
 
@@ -59,9 +93,29 @@ namespace SAM
 																	//! Class constructor
 																	TMatrix()
 																	{
+																		Identity();
+																	}
+
+																	//! overload the subscript operator
+		TVector<TYPE, 3>											&operator[] (const unsigned int index)
+																	{
+																		return m_element[index];
+																	}
+
+																	//! Change the matrix to an identity matrix
+		void														Identity()
+																	{																
 																		unsigned int columnIndex = 0;
 																		for (int rowIndex = 0; rowIndex < 3; ++rowIndex)
 																			m_element[rowIndex][columnIndex++] = static_cast<TYPE>(1);
+																	}
+
+																	//! Change the matrix to a zero matrix
+		void														Zero()
+																	{
+																		for (int rowIndex = 0; rowIndex < 3; ++rowIndex)
+																			for (int columnIndex = 0; columnIndex < 3; ++columnIndex)
+																				m_element[rowIndex][columnIndex] = static_cast<TYPE>(0);
 																	}
 	};
 
@@ -77,9 +131,29 @@ namespace SAM
 																	//! Class constructor
 																	TMatrix()
 																	{
+																		Identity();
+																	}
+
+																	//! overload the subscript operator
+		TVector<TYPE, 4>											&operator[] (const unsigned int index)
+																	{
+																		return m_element[index];
+																	}
+
+																	//! Change the matrix to an identity matrix
+		void														Identity()
+																	{																
 																		unsigned int columnIndex = 0;
 																		for (int rowIndex = 0; rowIndex < 4; ++rowIndex)
 																			m_element[rowIndex][columnIndex++] = static_cast<TYPE>(1);
+																	}
+
+																	//! Change the matrix to a zero matrix
+		void														Zero()
+																	{
+																		for (int rowIndex = 0; rowIndex < 4; ++rowIndex)
+																			for (int columnIndex = 0; columnIndex < 4; ++columnIndex)
+																				m_element[rowIndex][columnIndex] = static_cast<TYPE>(0);
 																	}
 	};
 
