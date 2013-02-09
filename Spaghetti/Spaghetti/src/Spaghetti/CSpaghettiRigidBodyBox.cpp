65cfd0f806152d5edf61d7b4940b34a095f2ca62
Sam Oates <sam@samoatesgames.com>
2013-02-09 02:30:41 +0000

    Wens way... is wank.

--- Spaghetti/Spaghetti/src/Spaghetti/CSpaghettiRigidBodyBox.cpp
+++ Spaghetti/Spaghetti/src/Spaghetti/CSpaghettiRigidBodyBox.cpp
@@ -51,7 +51,7 @@ void CSpaghettiRigidBodyBox::UpdateVelocity(
 	AddForceAtPoint(world->GetGravity(), m_position);
 
 	// update velocity
-	m_velocity = m_velocity + ((m_force / m_mass) * deltaTime);
+	m_velocity = m_velocity + (m_force * deltaTime);
 
 	// update the position of the body
 	m_lastPosition = m_position;
@@ -107,56 +107,31 @@ void CSpaghettiRigidBodyBox::HandleCollision(
 	SetPosition(GetLastPosition());
 	otherRigidBody->SetPosition(otherRigidBody->GetLastPosition());
 
-	static const float e = -0.0f;
+	static const float e = 1.2f;
 	Ogre::Vector3 relativeVelocity = GetVelocity() - otherRigidBody->GetVelocity();
 
 	// linear
 	{
-		Ogre::Vector3 thisVelocity = Ogre::Vector3::ZERO;
-		Ogre::Vector3 otherVelocity = Ogre::Vector3::ZERO;
+		const float inverseMassBodyOne = 1.0f / GetMass();
+		const float inverseMassBodyTwo = 1.0f / otherRigidBody->GetMass();
+		const float sumOfInverseMass = inverseMassBodyOne + inverseMassBodyTwo;
 
-		unsigned int noofCollisions = collisions.size();
-		for (unsigned int collisionIndex = 0; collisionIndex < noofCollisions; ++collisionIndex)
+		const unsigned int noofCollision = collisions.size();
+		for (unsigned int collisionIndex = 0; collisionIndex < noofCollision; ++collisionIndex)
 		{
 			Ogre::Vector3 collisionNormal = collisions[collisionIndex].collisionNormal;
-			Ogre::Vector3 collisionPoint = collisions[collisionIndex].collisionPoint;
-			collisionNormal.normalise();
-
-			const float inverseMassBodyOne = 1.0f / GetMass();
-			const float inverseMassBodyTwo = 1.0f / otherRigidBody->GetMass();
-			const float sumOfInverseMass = inverseMassBodyOne + inverseMassBodyTwo;
 			const float jLinear = (-(1.0f + e) * relativeVelocity.dotProduct(collisionNormal)) / sumOfInverseMass;
 
-			thisVelocity = thisVelocity + (collisionNormal * jLinear);
-			otherVelocity = otherVelocity + (collisionNormal * -jLinear);
-		}		
+			Ogre::Vector3 bodyOneImpulse = (jLinear * collisionNormal) * inverseMassBodyOne;
+			Ogre::Vector3 bodyTwoImpulse = (-jLinear * collisionNormal) * inverseMassBodyTwo;
 
-		if (noofCollisions != 0)
-		{
-			SetVelocity(thisVelocity / noofCollisions);
-			otherRigidBody->SetVelocity(otherVelocity / noofCollisions);
+			SetVelocity(GetVelocity() + bodyOneImpulse);
+			otherRigidBody->SetVelocity(otherRigidBody->GetVelocity() + bodyTwoImpulse);
 		}
-	}	
+	}		
 
 	// angular
 	{
-		//unsigned int noofCollisions = collisions.size();
-		//for (unsigned int collisionIndex = 0; collisionIndex < noofCollisions; ++collisionIndex)
-		//{
-		//	SAM::TVector3 collisionNormal = collisions[collisionIndex].collisionNormal;
-		//	SAM::TVector3 collisionPoint = collisions[collisionIndex].collisionPoint;
-		//	collisionNormal.Normalize();
 
-		//	SAM::TVector3 r1 = collisionPoint - GetPosition();
-		//	SAM::TVector3 r2 = collisionPoint - otherRigidBody->GetPosition();
-
-		//	SAM::TVector3 ac = (GetInverseInertia() * r1.Cross(collisionNormal)).Cross(r1);
-		//	SAM::TVector3 bc = (otherRigidBody->GetInverseInertia() * r2.Cross(collisionNormal)).Cross(r2);
-		//	const float nDc = collisionNormal.Dot(ac) + collisionNormal.Dot(bc);
-		//	const float jAngular = -(((1.0f + e) * relativeVelocity.Dot(collisionNormal)) * nDc);
-
-		//	SAM::TVector3 thisAngVel = (GetInverseInertia() * r1.Cross(collisionNormal * jAngular)) / static_cast<float>(noofCollisions);
-		//	SAM::TVector3 otherAngVel = (otherRigidBody->GetInverseInertia() * r2.Cross(collisionNormal * -jAngular)) / static_cast<float>(noofCollisions);
-		//}
 	}
 }
